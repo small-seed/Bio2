d0957119b6b62d43a0353c908b8a98bafbe12357
A step to the new world <97878200+small-seed@users.noreply.github.com>
2025-11-08 10:48:19 +0200

    Update Solution2_Expectation_Maximization.py

--- Probabilistic_Motif_and_Stochastic_Algorithms/Solution2_Expectation_Maximization.py
+++ Probabilistic_Motif_and_Stochastic_Algorithms/Solution2_Expectation_Maximization.py
@@ -1,128 +1,122 @@
-# -*- coding: utf-8 -*-
-from Solution1_Representing_and_Searching_Probabilistic_Motifs import MyMotifs
-from Solution1_Representing_and_Searching_Probabilistic_Motifs import MySeq
+from solution1_sequence_motif_handler import SequenceMotif
+from solution1_sequence_motif_handler import BioSequence
+import random
 
-class MotifFinding:
-
-    def __init__(self, size=8, seqs=None):
-        self.motif_size = size
-        if (seqs != None):
-            self.seqs = seqs
-            self.alphabet = seqs[0].alphabet()
-        else:
-            self.seqs = []
+class MotifDiscovery:
+    def __init__(self, motif_length=8, sequences=None):
+        self.motif_length = motif_length
+        self.sequences = sequences if sequences is not None else []
+        if self.sequences:
+            self.base_alphabet = self.sequences[0].get_alphabet()
 
     def __len__(self):
-        return len(self.seqs)
+        return len(self.sequences)
 
-    def __getitem__(self, n):
-        return self.seqs[n]
+    def __getitem__(self, index):
+        return self.sequences[index]
 
-    def seq_size(self, i):
-        return len(self.seqs[i])
+    def get_sequence_length(self, index):
+        return len(self.sequences[index])
 
-    def read_file(self, fic, t):
-        for s in open(fic, "r"):
-            self.seqs.append(MySeq(s.strip().upper(), t))
-        self.alphabet = self.seqs[0].alphabet()
+    def load_sequences_from_file(self, file_path, seq_type):
+        with open(file_path, 'r') as file_handle:
+            for line in file_handle:
+                stripped_line = line.strip().upper()
+                if stripped_line:  # Skip empty lines
+                    self.sequences.append(BioSequence(stripped_line, seq_type))
+        if self.sequences:
+            self.base_alphabet = self.sequences[0].get_alphabet()
 
-    def create_motif_from_indexes(self, indexes):
-        pseqs = []
-        for i, ind in enumerate(indexes):
-            pseqs.append(MySeq(self.seqs[i][ind:(ind + self.motif_size)], self.seqs[i].get_seq_biotype()))
-        return MyMotifs(pseqs)
+    def build_motif_from_positions(self, position_list):
+        extracted_subseqs = []
+        for idx, start_pos in enumerate(position_list):
+            subseq_str = self.sequences[idx][start_pos:start_pos + self.motif_length]
+            extracted_subseqs.append(BioSequence(subseq_str, self.sequences[idx].sequence_type()))
+        return SequenceMotif(extracted_subseqs)
 
-    # SCORES (include in deterministic motif finding)
+    def compute_additive_score(self, position_list):
+        motif_instance = self.build_motif_from_positions(position_list)
+        motif_instance._compute_counts()
+        count_table = motif_instance.count_matrix
+        total_score = 0
+        num_positions = len(count_table[0]) if count_table else 0
+        num_bases = len(count_table)
+        for col in range(num_positions):
+            max_in_col = count_table[0][col]
+            for row in range(1, num_bases):
+                if count_table[row][col] > max_in_col:
+                    max_in_col = count_table[row][col]
+            total_score += max_in_col
+        return total_score
 
-    def score(self, s):
-        score = 0
-        motif = self.create_motif_from_indexes(s)
-        motif.do_counts()
-        mat = motif.counts
-        for j in range(len(mat[0])):
-            maxcol = mat[0][j]
-            for i in range(1, len(mat)):
-                if mat[i][j] > maxcol:
-                    maxcol = mat[i][j]
-            score += maxcol
-        return score
+    def compute_product_score(self, position_list):
+        motif_instance = self.build_motif_from_positions(position_list)
+        motif_instance._compute_weight_matrix()
+        prob_table = motif_instance.weight_matrix
+        product_score = 1.0
+        num_positions = len(prob_table[0]) if prob_table else 0
+        num_bases = len(prob_table)
+        for col in range(num_positions):
+            max_in_col = prob_table[0][col]
+            for row in range(1, num_bases):
+                if prob_table[row][col] > max_in_col:
+                    max_in_col = prob_table[row][col]
+            product_score *= max_in_col
+        return product_score
 
-    def score_multiplicative(self, s):
-        score = 1.0
-        motif = self.create_motif_from_indexes(s)
-        motif.create_pwm()
-        mat = motif.pwm
-        for j in range(len(mat[0])):
-            maxcol = mat[0][j]
-            for i in range(1, len(mat)):
-                if mat[i][j] > maxcol:
-                    maxcol = mat[i][j]
-            score *= maxcol
-        return score
-
-        # Probabilistic Motif Finding
-
-    # heuristic stochastic
-
-    def heuristic_stochastic(self):
-        from random import randint
-        s = [0] * len(self.seqs)
-        for k in range(len(s)):
-            s[k] = randint(0, self.seq_size(k) - self.motif_size)
-        motif = self.create_motif_from_indexes(s)
-        motif.create_pwm()
-        sc = self.score_multiplicative(s)
-        bestsol = s
-        improve = True
-        while (improve):
-            for k in range(len(s)):
-                s[k] = motif.most_probable_sequence(self.seqs[k])
-            if self.score_multiplicative(s) > sc:
-                sc = self.score_multiplicative(s)
-                bestsol = s
-                motif = self.create_motif_from_indexes(s)
-                motif.create_pwm()
+    def perform_heuristic_search(self):
+        current_positions = [random.randint(0, self.get_sequence_length(i) - self.motif_length)
+                             for i in range(len(self.sequences))]
+        current_motif = self.build_motif_from_positions(current_positions)
+        current_motif._compute_weight_matrix()
+        current_score = self.compute_product_score(current_positions)
+        best_positions = current_positions[:]
+        has_improved = True
+        while has_improved:
+            for seq_idx in range(len(self.sequences)):
+                current_positions[seq_idx] = current_motif.find_best_match_position(self.sequences[seq_idx])
+            new_score = self.compute_product_score(current_positions)
+            if new_score > current_score:
+                current_score = new_score
+                best_positions = current_positions[:]
+                current_motif = self.build_motif_from_positions(current_positions)
+                current_motif._compute_weight_matrix()
             else:
-                improve = False
-        return bestsol
+                has_improved = False
+        return best_positions
 
-    # gibbs sampling
+    # Gibbs sampling method
+    def execute_gibbs_sampling(self, num_iterations=100):
+        initial_positions = [random.randint(0, len(self.sequences[0]) - self.motif_length)
+                             for _ in range(len(self.sequences))]
+        best_positions = initial_positions[:]
+        best_score = self.compute_product_score(initial_positions)
+        for _ in range(num_iterations):
+            # Select a random sequence to resample
+            selected_seq_idx = random.randint(0, len(self.sequences) - 1)
+            selected_seq = self.sequences[selected_seq_idx]
+            # Temporarily remove the sequence
+            temp_positions = initial_positions[:selected_seq_idx] + initial_positions[selected_seq_idx + 1:]
+            temp_seqs = self.sequences[:selected_seq_idx] + self.sequences[selected_seq_idx + 1:]
+            temp_motif = self.build_motif_from_positions(temp_positions)
+            temp_motif._compute_weight_matrix()
+            # Compute probabilities for all positions in the selected sequence
+            pos_probs = temp_motif.calculate_site_probabilities(selected_seq)
+            # Select new position via roulette wheel
+            new_pos = self.roulette_wheel_selection(pos_probs)
+            initial_positions[selected_seq_idx] = new_pos
+            updated_score = self.compute_product_score(initial_positions)
+            if updated_score > best_score:
+                best_score = updated_score
+                best_positions = initial_positions[:]
+        return best_positions
 
-    def gibbs(self, iterations=100):
-        from random import randint
-        s = []
-        for i in range(len(self.seqs)):
-            s.append(randint(0, len(self.seqs[0]) - self.motif_size - 1))
-        best_s = list(s)
-        best_score = self.score_multiplicative(s)
-        for it in range(iterations):
-            # randomly pick a sequence
-            seq_idx = randint(0, len(self.seqs) - 1)
-            seq_sel = self.seqs[seq_idx]
-            s.pop(seq_idx)
-            removed = self.seqs.pop(seq_idx)
-            motif = self.create_motif_from_indexes(s)
-            motif.create_pwm()
-            self.seqs.insert(seq_idx, removed)
-            r = motif.probability_all_positions(seq_sel)
-            pos = self.roulette(r)
-            s.insert(seq_idx, pos)
-            score = self.score_multiplicative(s)
-            if score > best_score:
-                best_score = score
-                best_s = list(s)
-        return best_s
-
-    def roulette(self, f):
-        from random import random
-        tot = 0.0
-        for x in f: tot += (0.01 + x)
-        val = random() * tot
-        acum = 0.0
-        idx = 0
-        while acum < val:
-            acum += (f[idx] + 0.01)
-            idx += 1
-        return idx - 1
-
-
+    def roulette_wheel_selection(self, probabilities):
+        total_sum = sum(0.01 + prob for prob in probabilities)
+        random_value = random.random() * total_sum
+        cumulative = 0.0
+        for idx, prob in enumerate(probabilities):
+            cumulative += (prob + 0.01)
+            if cumulative >= random_value:
+                return idx
+        return len(probabilities) - 1
