098be0b240cb2533411bf3951a5cb936e71c80f4
A step to the new world <97878200+small-seed@users.noreply.github.com>
2025-11-08 10:03:58 +0200

    Update Solution6_Python_library.py

--- Pairwise_Sequence_Alignment/Solution6_Python_library.py
+++ Pairwise_Sequence_Alignment/Solution6_Python_library.py
@@ -2,35 +2,30 @@ from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio.SubsMat import MatrixInfo
 
-# Alignment of two DNA sequences, using match score of 1, mismatch and gap score are 0
-def global_Align_Python(adn1, adn2):
-    alignments = pairwise2.align.globalxx(adn1, adn2)
+def perform_global_alignment(seq1, seq2):
+    results = pairwise2.align.globalxx(seq1, seq2)
+    return results
 
-# Alignment of two DNA sequences using BLOSUM62 substitution matrix
-# gap score are -4 and extension penalty of -1
-def Align_using_BLOSUM62(prot1, prot2):
-    matrix = MatrixInfo.blosum62
-    for a in pairwise2.align.globalds(prot1, prot2, matrix, -4, -1):
-        print(format_alignment(*a))
+def execute_blosum_alignment(seq1, seq2):
+    scoring_matrix = MatrixInfo.blosum62
+    alignments = pairwise2.align.globalds(seq1, seq2, scoring_matrix, -4, -1)
+    for result in alignments:
+        print(format_alignment(*result))
 
-#perform local alignments: in the first case of DNA
-#sequences using a match score of 3, mismatch score of −2 and
-# constant gap penalty g of −3.
-def local_Align_Python(adn1, adn2, prot1, prot2):
-    matrix = MatrixInfo.blosum62
-    # Align DNA sequence
-    local_dna = pairwise2.align.localms(adn1, adn2, 3, -2, -3, -3)
-    # Align protein sequence
-    local_prot = pairwise2.align.localds(prot1, prot2, matrix, -4, -1)
-    return local_dna, local_prot
+def execute_local_alignments(dna_seq1, dna_seq2, prot_seq1, prot_seq2):
+    scoring_matrix = MatrixInfo.blosum62
+    dna_results = pairwise2.align.localms(dna_seq1, dna_seq2, 3, -2, -3, -3)
+    prot_results = pairwise2.align.localds(prot_seq1, prot_seq2, scoring_matrix, -4, -1)
+    return dna_results, prot_results
 
-def test():
-    seqAdn_1 = input("Input DNA sequence 1:")
-    seqAdn_2 = input("Input DNA enzyme 2:")
-    seqPr_1 = input("Input protein sequence 1:")
-    seqPr_2 = input("Input protein enzyme 2:")
-    alignment = local_Align_Python(seqAdn_1, seqAdn_2, seqPr_1, seqPr_2)
-    print("ADN alignment result is ", alignment[0])
-    print("ADN alignment result is ", alignment[1])
+def run_example():
+    dna1 = input("Enter first DNA sequence: ")
+    dna2 = input("Enter second DNA sequence: ")
+    prot1 = input("Enter first protein sequence: ")
+    prot2 = input("Enter second protein sequence: ")
+    results = execute_local_alignments(dna1, dna2, prot1, prot2)
+    print("DNA local alignment:", results[0])
+    print("Protein local alignment:", results[1])
 
-test()
+if __name__ == "__main__":
+    run_example()
