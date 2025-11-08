fa7ee83572d90a1195ad3128e91885afb96c146d
A step to the new world <97878200+small-seed@users.noreply.github.com>
2025-11-08 11:04:01 +0200

    Update Solution1_Simple_BLAST.py

--- Search_sequence_in_Database/Solution1_Simple_BLAST.py
+++ Search_sequence_in_Database/Solution1_Simple_BLAST.py
@@ -1,91 +1,96 @@
-# read from file
-def read_database(filename):
-    f = open(filename)
-    db = []
-    for line in f:
-        db.append(line.rstrip())
-    f.close()
-    return db
+# Function to load sequences from a file into a list
+def load_sequences_from_file(file_path):
+    sequences = []
+    with open(file_path, 'r') as file_handle:
+        for line in file_handle:
+            sequences.append(line.strip())
+    return sequences
 
-# Hash map to identify all words of size w (a parameter) occurring in query sequence
-# The words in the query sequence as key, and a list of occurrence position as values
-def build_hash_map(query, width):
-    res = {}
-    for i in range(len(query)-width+1):
-        subseq = query[i:i+width]
-    if subseq in res:
-        res[subseq].append(i)
-    else:
-        res[subseq] = [i]
-    return res
+# Function to create a hash table of k-mers from the query sequence
+def create_kmer_index(query_sequence, kmer_length):
+    kmer_to_positions = {}
+    query_len = len(query_sequence)
+    for start_idx in range(query_len - kmer_length + 1):
+        kmer = query_sequence[start_idx:start_idx + kmer_length]
+        if kmer in kmer_to_positions:
+            kmer_to_positions[kmer].append(start_idx)
+        else:
+            kmer_to_positions[kmer] = [start_idx]
+    return kmer_to_positions
 
-# find all the matches of words from a sequence with the query
-# return a list of hit in tuple type: index of match in query and
-# index of match in sequence
-def hit_matches (seq, hashedmap, width):
-    res = [] # list of tuples
-    for i in range(len(seq)-width+1):
-        subseq = seq[i:i+width]
-        if subseq in hashedmap:
-            l = hashedmap[subseq]
-            for ind in l:
-                res.append((ind, i))
-    return res
+# Function to identify exact k-mer matches between a sequence and the query index
+def find_kmer_hits(target_sequence, kmer_index, kmer_length):
+    matches = []
+    target_len = len(target_sequence)
+    for start_idx in range(target_len - kmer_length + 1):
+        kmer = target_sequence[start_idx:start_idx + kmer_length]
+        if kmer in kmer_index:
+            for query_start in kmer_index[kmer]:
+                matches.append((query_start, start_idx))
+    return matches
 
-# from the words in the list, extend the word to both direction
-# The result is the tuple: start index in query,start index in sequence
-# size of the alignment, and the score
-def extends_hit (seq, hit, query, width):
-    str_query, str_sequence = hit[0], hit[1]
-    #extend foward direction
-    matfw = 0
-    k = 0
-    bestk = 0
-    while 2*matfw >= k and str_query+width+k < len(query) and str_sequence+width+k < len(seq):
-        if query[str_query+width+k] == seq[str_sequence+width+k]:
-            matfw += 1
-            bestk = k+1
-            k += 1
-    size = width + bestk
-    # move backwards
-    k = 0
-    matbw = 0
-    bestk = 0
-    while 2*matbw >= k and str_query > k and str_sequence > k:
-        if query[str_query-k-1] == seq[str_sequence-k-1]:
-            matbw += 1
-            bestk = k + 1
-            k += 1
-            size += bestk
-    return (str_query-bestk, str_sequence-bestk, size, width+matfw+matbw)
+# Function to extend a seed match bidirectionally using a 50% identity threshold
+def extend_seed_match(target_sequence, seed_hit, query_sequence, kmer_length):
+    query_start, target_start = seed_hit
+    forward_matches = 0
+    forward_length = 0
+    max_forward = 0
+    while (query_start + kmer_length + forward_length < len(query_sequence) and
+           target_start + kmer_length + forward_length < len(target_sequence)):
+        if query_sequence[query_start + kmer_length + forward_length] == \
+           target_sequence[target_start + kmer_length + forward_length]:
+            forward_matches += 1
+        forward_length += 1
+        if 2 * forward_matches >= forward_length:
+            max_forward = forward_length
+        else:
+            break
+    
+    backward_matches = 0
+    backward_length = 0
+    max_backward = 0
+    while (query_start > backward_length and target_start > backward_length):
+        if query_sequence[query_start - 1 - backward_length] == \
+           target_sequence[target_start - 1 - backward_length]:
+            backward_matches += 1
+        backward_length += 1
+        if 2 * backward_matches >= backward_length:
+            max_backward = backward_length
+        else:
+            break
+    total_length = kmer_length + max_forward + max_backward
+    total_matches = kmer_length + forward_matches + backward_matches  # Assuming seed is perfect match
+    adjusted_query_start = query_start - max_backward
+    adjusted_target_start = target_start - max_backward
+    return (adjusted_query_start, adjusted_target_start, total_length, total_matches)
 
-# Find the longest match or best score of each words in the initial list
-def hit_best_score(seq, query, hashedmap, width):
-    hits = hit_matches(seq, hashedmap, width)
-    bestScore = -1.0
-    # create a tuple to store results
-    best = ()
-    for h in hits:
-        ext = extends_hit(seq, h, query, width)
-        score = ext[3]
-        if score > bestScore or (score == bestScore and ext[2] < best[2]):
-            bestScore = score
-            best = ext
-    return best
-
-# Compare all the best results from function above and find the final best result
-def best_alignment (db, query, w):
-    m = build_hash_map(query, w)
-    bestScore = -1.0
-    res = (0, 0, 0, 0, 0)
-    for k in range(0, len(db)):
-        bestSeq = hit_best_score(db[k], query, m, w)
-        if bestSeq != ():
-            score = bestSeq[3]
-            if score > bestScore or (score == bestScore and bestSeq[2] < res[2]):
-                bestScore = score
-                res = bestSeq[0], bestSeq[1], bestSeq[2], bestSeq[3], k
-    if bestScore < 0:
+# Function to find the best extended hit for a single target sequence
+def find_best_hit_for_sequence(target_sequence, query_sequence, kmer_index, kmer_length):
+    seed_hits = find_kmer_hits(target_sequence, kmer_index, kmer_length)
+    if not seed_hits:
         return ()
-    else:
-        return res
+    best_score = -1
+    best_alignment = ()
+    for hit in seed_hits:
+        extended = extend_seed_match(target_sequence, hit, query_sequence, kmer_length)
+        current_score = extended[3]
+        if (current_score > best_score or
+            (current_score == best_score and extended[2] < best_alignment[2])):
+            best_score = current_score
+            best_alignment = extended
+    return best_alignment
+
+# Main function to search a database for the best alignment to the query
+def search_database_for_best_alignment(sequence_database, query_sequence, kmer_length):
+    kmer_index = create_kmer_index(query_sequence, kmer_length)
+    global_best_score = -1
+    global_best = (0, 0, 0, 0, 0)
+    for db_index, db_sequence in enumerate(sequence_database):
+        candidate = find_best_hit_for_sequence(db_sequence, query_sequence, kmer_index, kmer_length)
+        if candidate:
+            candidate_score = candidate[3]
+            if (candidate_score > global_best_score or
+                (candidate_score == global_best_score and candidate[2] < global_best[2])):
+                global_best_score = candidate_score
+                global_best = (*candidate, db_index)
+    return global_best if global_best_score >= 0 else ()
