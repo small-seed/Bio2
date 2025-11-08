ac00090432b2387d22308701a20666436b194e31
A step to the new world <97878200+small-seed@users.noreply.github.com>
2025-11-08 09:54:43 +0200

    Update Solution1_Simple_Progressive_MSA_Part_II.py

--- Multiple_Sequence_Alignment/Solution1_Simple_Progressive_MSA_Part_II.py
+++ Multiple_Sequence_Alignment/Solution1_Simple_Progressive_MSA_Part_II.py
@@ -1,46 +1,71 @@
-from Solution1_Simple_Progressive_MSA_Part_I import PairwiseAlignment
-from Solution1_Simple_Progressive_MSA_Part_I import MyAlign
-from Solution1_Simple_Progressive_MSA_Part_I import MySeq
-from Solution1_Simple_Progressive_MSA_Part_I import SubstMatrix
+```python
+from Solution1_Simple_Progressive_MSA_Part_I import PairwiseAligner as PairwiseAlignment
+from Solution1_Simple_Progressive_MSA_Part_I import Alignment as MyAlign
+from Solution1_Simple_Progressive_MSA_Part_I import Sequence as MySeq
+from Solution1_Simple_Progressive_MSA_Part_I import SubstitutionMatrix as SubstMatrix
 
-class MultipleAlignment():
-    def __init__(self, seqs, alignseq: PairwiseAlignment):
-        self.seqs = seqs
-        self.alignpars = alignseq
 
-    def add_sequence_alignment(self, alignment: MyAlign, sequence):
-        res = []
-        for i in range(len(alignment.listOfSeqs)+1):
-            res.append("")
-        cons = MySeq(alignment.concecus(), alignment.al_type)
-        self.alignpars.needleman_Wunsh_Algorithsm(cons, sequence)
-        align2 = self.alignpars.recover_align()
-        orig = 0
-        for i in range(len(align2)):
-            if align2[0, i] == '−':
-                for k in range(len(alignment.listOfSeqs)):
-                    res[k] += "−"
+class ProgressiveMultipleAligner:
+    
+    def __init__(self, input_sequences, pairwise_aligner: PairwiseAlignment):
+        self.input_sequences = input_sequences
+        self.aligner = pairwise_aligner
+    
+    def _align_to_current(self, current_alignment: MyAlign, new_sequence: MySeq):
+        num_existing = current_alignment.num_sequences()
+        new_aligned_rows = [""] * (num_existing + 1)
+        
+        # Compute consensus sequence as guide
+        consensus_guide = MySeq(current_alignment.consensus(), current_alignment.alignment_type)
+        
+        # Align consensus to new sequence
+        self.aligner.needleman_wunsch(consensus_guide, new_sequence)
+        new_pair_align = self.aligner.recover_global_alignment()
+        
+        source_col = 0
+        for pos in range(len(new_pair_align)):
+            if new_pair_align[0, pos] == '-':
+                # Insert gap in all existing sequences
+                for row in range(num_existing):
+                    new_aligned_rows[row] += '-'
             else:
-                for k in range(len(alignment.listOfSeqs)):
-                    res[k] += alignment[k, orig]
-                orig += 1
-        res[len(alignment.listOfSeqs)] = align2.listseqs[1]
-        return MyAlign(res, alignment.listOfSeqs)
+                # Copy column from current alignment
+                for row in range(num_existing):
+                    new_aligned_rows[row] += current_alignment[row, source_col]
+                source_col += 1
+        
+        # Add the aligned new sequence
+        new_aligned_rows[num_existing] = new_pair_align[1]
+        
+        return MyAlign(new_aligned_rows, current_alignment.alignment_type)
+    
+    def perform_alignment(self):
+        if len(self.input_sequences) < 2:
+            return MyAlign([str(seq) for seq in self.input_sequences], self.input_sequences[0].get_type() if self.input_sequences else "DNA")
+        
+        # Initial pairwise alignment of first two sequences
+        self.aligner.needleman_wunsch(self.input_sequences[0], self.input_sequences[1])
+        current_alignment = self.aligner.recover_global_alignment()
+        
+        # Iteratively add remaining sequences
+        for seq_idx in range(2, len(self.input_sequences)):
+            current_alignment = self._align_to_current(current_alignment, self.input_sequences[seq_idx])
+        
+        return current_alignment
 
-    def align_consensus(self):
-        self.alignpars.needleman_Wunsh_Algorithsm(self.seqs[0], self.seqs[1])
-        res = self.alignpars.recover_align()
-        for i in range(2, len(self.seqs)):
-            res = self.add_sequence_alignment(res, self .seqs[i])
-        return res
 
-def test():
-    s1 = MySeq("ATAGC")
-    s2 = MySeq("AACC")
-    s3 = MySeq("ATGAC")
-    sm = SubstMatrix()
-    sm.create_submat(1, -1, "ACGT")
-    aseq = PairwiseAlignment(sm, -1)
-    ma = MultipleAlignment([s1, s2, s3], aseq)
-    al = ma.align_consensus()
-    print(al)
+def run_example():
+    seq1 = MySeq("ATAGC")
+    seq2 = MySeq("AACC")
+    seq3 = MySeq("ATGAC")
+    
+    sub_matrix = SubstMatrix()
+    sub_matrix.create_simple_matrix(match_score=1, mismatch_score=-1, alphabet=list("ACGT"))
+    
+    pair_aligner = PairwiseAlignment(sub_matrix, gap_penalty=-1)
+    
+    multi_aligner = ProgressiveMultipleAligner([seq1, seq2, seq3], pair_aligner)
+    final_alignment = multi_aligner.perform_alignment()
+    
+    print(final_alignment)
+```
